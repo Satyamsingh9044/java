70f96361fe15a501deabb1d8dfcdfa9d0f839075
satyam9044 <satyamsingh424624@gamil.com>
2026-03-12 19:18:40 +0530

    commenting code

--- LinkedList/RotateList.java
+++ LinkedList/RotateList.java
@@ -1,24 +1,24 @@
- public class RotateList { //lc-61
-    public static  ListNode rotateRight(ListNode head, int k) {
-        if(head==null||head.next==null||k==0) return head;
-        ListNode temp1=head;
-        ListNode temp2=head;
-        int length=1;
-        while(temp1.next!=null){
-            temp1=temp1.next;
-            length++;
-        }
-        k=k%length;
-        if(k==0) return head;
-        int i=1;
-        while(i<length-k){
-            temp2=temp2.next;
-            i++;
-        }
-        ListNode temp3=temp2.next;
-        temp2.next=null;
-        temp1.next=head;
-        head=temp3;
-        return head;
-    }
-}
+//  public class RotateList { //lc-61
+//     public static  ListNode rotateRight(ListNode head, int k) {
+//         if(head==null||head.next==null||k==0) return head;
+//         ListNode temp1=head;
+//         ListNode temp2=head;
+//         int length=1;
+//         while(temp1.next!=null){
+//             temp1=temp1.next;
+//             length++;
+//         }
+//         k=k%length;
+//         if(k==0) return head;
+//         int i=1;
+//         while(i<length-k){
+//             temp2=temp2.next;
+//             i++;
+//         }
+//         ListNode temp3=temp2.next;
+//         temp2.next=null;
+//         temp1.next=head;
+//         head=temp3;
+//         return head;
+//     }
+// }
