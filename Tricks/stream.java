93cecf38bf1ca3c85f295d7a83bb21a5579f8794
satyam9044 <satyamsingh424624@gmail.com>
2026-08-27 10:10:30 +0530

    new Tricks

---
+++ Tricks/stream.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class stream{
+    public static void main(String[] args){
+        int arr[]={4,6,3,8,1,99,33,66};
+        Arrays.stream(arr)
+        .sorted()
+        .forEach(f->System.out.print(f+" "));
+    }
+}
