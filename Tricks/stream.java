19dfdbc65a4f47bac4c20b399f459fe5d93b796b
satyam9044 <satyamsingh424624@gmail.com>
2026-08-31 07:13:48 +0530

    update stream.java

--- Tricks/stream.java
+++ Tricks/stream.java
@@ -4,7 +4,7 @@ public class stream{
     public static void main(String[] args){
         int arr[]={4,6,3,8,1,99,33,66};
         Arrays.stream(arr)
-        .sorted()
+        // .sorted()
         .forEach(f->System.out.print(f+" "));
     }
 }
